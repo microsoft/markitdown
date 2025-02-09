7a6a08b3a178e27388e4e8de8641acd5b5fc61d2
Adam Fourney <adamfo@microsoft.com>
2025-02-09 11:38:47 -0800

    More converters.

--- src/markitdown/converters/__init__.py
+++ src/markitdown/converters/__init__.py
@@ -5,10 +5,16 @@
 from ._base import DocumentConverter, DocumentConverterResult
 from ._plain_text_converter import PlainTextConverter
 from ._html_converter import HtmlConverter
+from ._rss_converter import RssConverter
+from ._wikipedia_converter import WikipediaConverter
+from ._youtube_converter import YouTubeConverter
 
 __all__ = [
     "DocumentConverter",
     "DocumentConverterResult",
     "PlainTextConverter",
     "HtmlConverter",
+    "RssConverter",
+    "WikipediaConverter",
+    "YouTubeConverter",
 ]
