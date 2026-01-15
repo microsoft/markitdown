4d7b65521c366b1cdf77a5ddf0e2b2ea1ac53756
Bar Pin <1286345540@qq.com>
2026-01-15 15:03:37 +0800

    Refine EPUB image test

--- packages/markitdown/tests/test_epub_images.py
+++ packages/markitdown/tests/test_epub_images.py
@@ -1,22 +1,20 @@
-import base64
-import os
 import re
 
 from markitdown import MarkItDown
 
 
-def test_epub_conversion():
+def test_epub_image_conversion():
     markitdown = MarkItDown()
     result = markitdown.convert(
-        "test_files/piper-temple-trouble.epub",
+        "test_files/test_epub_images.epub",
         keep_data_uris=True,
     )
     md = result.markdown
     image_re = re.compile(pattern=r"!\[(.*?)\]\(data:image/(.*?);base64,(.*?)\)")
     images = image_re.findall(md)
 
-    print(f"get images num: {len(images)}")
+    assert len(images) > 0
 
 
 if __name__ == "__main__":
-    test_epub_conversion()
+    test_epub_image_conversion()
