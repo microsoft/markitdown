2b40060a55db2236cd1c6a8c79613db60ad25b0c
venti <1308199824@qq.com>
2026-05-30 15:35:58 +0800

    fix: catch UnicodeDecodeError in IpynbConverter.accepts()

--- packages/markitdown/src/markitdown/converters/_ipynb_converter.py
+++ packages/markitdown/src/markitdown/converters/_ipynb_converter.py
@@ -38,6 +38,8 @@ class IpynbConverter(DocumentConverter):
                         "nbformat" in notebook_content
                         and "nbformat_minor" in notebook_content
                     )
+                except (UnicodeDecodeError, ValueError):
+                    return False
                 finally:
                     file_stream.seek(cur_pos)
 
