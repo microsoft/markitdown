9ef0a2c36b5971ec45e6491f264fc21fe047fe99
venti <1308199824@qq.com>
2026-05-30 16:17:47 +0800

    fix: decode exiftool JSON output as UTF-8 instead of locale encoding (fixes #1972)

--- packages/markitdown/src/markitdown/converters/_exiftool.py
+++ packages/markitdown/src/markitdown/converters/_exiftool.py
@@ -1,5 +1,4 @@
 import json
-import locale
 import subprocess
 from typing import Any, BinaryIO, Union
 
@@ -46,7 +45,7 @@ def exiftool_metadata(
         ).stdout
 
         return json.loads(
-            output.decode(locale.getpreferredencoding(False)),
+            output.decode("utf-8"),
         )[0]
     finally:
         file_stream.seek(cur_pos)
